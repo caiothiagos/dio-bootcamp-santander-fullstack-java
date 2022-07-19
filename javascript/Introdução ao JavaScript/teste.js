e62cb7be38c1ad78ab900e738133deecd24817ee
Caio Thiago <caiothiagos@hotmail.com>
2022-07-19 16:41:08 -0300

    Introdução ao JavaScript - Funções

--- javascript/Introdução ao JavaScript/teste.js
+++ javascript/Introdução ao JavaScript/teste.js
@@ -5,9 +5,16 @@ var x = 1;*/
 //ar z = 2
 
 //var preco = 2;
-var desconto = 0.2;
+/*var desconto = 0.2;
 //preco = preco * desconto;
 
 const PRECO = 2;
 var total  = PRECO - desconto;
+*/
 
+function soma(a, b){
+    console.log(a +b);
+    return a +b;
+}
+
+soma(3,5); // 8
