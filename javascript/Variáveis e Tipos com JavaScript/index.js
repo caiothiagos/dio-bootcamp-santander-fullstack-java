4bf549dc3b8a361afe921511467411e8f7cda9b9
Caio Thiago <caiothiagos@hotmail.com>
2022-07-20 11:16:27 -0300

    Variáveis e Tipos com JavaScript - Variáveis

---
+++ javascript/Variáveis e Tipos com JavaScript/index.js
@@ -0,0 +1,23 @@
+/*
+numberOne = 1;
+console.log(numberOne + 2);
+var numberOne;
+*/
+
+/*
+numberOne = 1;
+console.log(numberOne + 2);
+let numberOne;
+*/
+
+var firstName = 'João';
+var lastName = 'Souza';
+if(firstName === 'João'){
+    let lastName = 'Rodrigues';
+    var firstName = 'Pedro';
+    lastName = 'Silva';
+
+    console.log(lastName);
+}
+
+console.log(firstName, lastName);
