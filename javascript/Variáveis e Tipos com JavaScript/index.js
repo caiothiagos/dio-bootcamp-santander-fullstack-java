d6da324ba7df60e061bb1d2650c59f1b7afab2c9
Caio Thiago <caiothiagos@hotmail.com>
2022-07-20 13:49:18 -0300

    Variáveis e Tipos com JavaScript - Strings

--- javascript/Variáveis e Tipos com JavaScript/index.js
+++ javascript/Variáveis e Tipos com JavaScript/index.js
@@ -42,9 +42,71 @@ reatribuir      sim                 não         sim
 hoisting        sim                 não         não
 */
 
+/*
 let um = "um";
 um
 typeof um
 
 um = 1
 typeof um
+*/
+
+let nome = "João"
+nome
+
+let sobrenome = "Pedro"
+typeof nome
+
+nome.concat(sobrenome)
+
+let concatenado = nome.concat(sobrenome)
+
+typeof concatenado
+
+concatenado.length
+
+let exemplo1 = "blablabla"
+let exemplo2= new String("blablabla")
+
+exemplo1.length
+
+nome
+
+nome[1]
+
+nome.length
+
+concatenado
+
+concatenado = nome + " " + sobrenome
+
+concatenado
+
+concatenado = nome + "\n" + sobrenome
+
+concatenado = `${nome} ${sobrenome}`
+
+concatenado = `${nome}
+
+${sobrenome}`
+
+
+let frase = "Olá, tudo bem?"
+
+frase.split("")
+
+frase.includes("tudo")
+
+frase.startsWith("O")
+
+frase.startsWith("R")
+
+frase.endsWith("?")
+
+frase.replace(",", "!")
+
+let stringModificada = frase.replace(",", "!")
+
+frase
+
+stringModificada
