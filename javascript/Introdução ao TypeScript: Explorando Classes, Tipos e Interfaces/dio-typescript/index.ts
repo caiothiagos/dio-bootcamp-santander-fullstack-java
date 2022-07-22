419d892f4cbd6aa8ea7c3f6022386a96f05384ec
Caio Thiago <caiothiagos@hotmail.com>
2022-07-22 13:15:49 -0300

    Introdução ao TypeScript: Explorando Classes, Tipos e Interfaces - Como funcionam as interfaces

--- javascript/Introdução ao TypeScript: Explorando Classes, Tipos e Interfaces/dio-typescript/index.ts
+++ javascript/Introdução ao TypeScript: Explorando Classes, Tipos e Interfaces/dio-typescript/index.ts
@@ -5,3 +5,34 @@ function soma(a: number, b: number){
 }
 
 soma(5, 7)
+
+//types
+//interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'áquatico';
+    
+    executarRugido(alturaEmDecibeis: number): void;
+}
+
+interface IFelino extends IAnimal{
+    visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+animal.executarRugido(10)
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+felino.executarRugido(10)
