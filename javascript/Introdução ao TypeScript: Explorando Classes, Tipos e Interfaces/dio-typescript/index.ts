fbbcd49ccfb8cd21ef2a059d93714d26757c1a44
Caio Thiago <caiothiagos@hotmail.com>
2022-07-22 13:29:25 -0300

    Introdução ao TypeScript: Explorando Classes, Tipos e Interfaces - Como funcionam os types

--- javascript/Introdução ao TypeScript: Explorando Classes, Tipos e Interfaces/dio-typescript/index.ts
+++ javascript/Introdução ao TypeScript: Explorando Classes, Tipos e Interfaces/dio-typescript/index.ts
@@ -9,30 +9,56 @@ soma(5, 7)
 //types
 //interfaces
 
+
 interface IAnimal {
     nome: string;
     tipo: 'terrestre' | 'áquatico';
-    
+    domestico: boolean;
+    // executarRugido(alturaEmDecibeis: number): void;
+}
+
+/*
+type IAnimal = {
+    nome: string;
+    tipo: 'terrestre' | 'áquatico';
     executarRugido(alturaEmDecibeis: number): void;
 }
+*/
 
 interface IFelino extends IAnimal{
     visaoNoturna: boolean;
 }
 
+interface ICanino extends IAnimal{
+    porte: 'pequeno' | 'medio' | 'grande'
+}
+
+type IDomestico = IFelino | ICanino
+//type IDomestico = IFelino & ICanino
+
+
 const animal: IAnimal = {
     nome: 'Elefante',
     tipo: 'terrestre',
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+    domestico: false
+    // executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
 }
 
-animal.executarRugido(10)
+//animal.executarRugido(10)
 
 const felino: IFelino = {
     nome: 'Leão',
     tipo: 'terrestre',
+    domestico: false,
     visaoNoturna: true,
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+    // executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
 }
 
-felino.executarRugido(10)
+//felino.executarRugido(10)
+
+const animalD: IDomestico = {
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'medio',
+    tipo: 'terrestre'
+}
