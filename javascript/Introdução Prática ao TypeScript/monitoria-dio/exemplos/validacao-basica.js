ec695cac65757e68e2c2858097b675d8e30b4d80
Caio Thiago <caiothiagos@hotmail.com>
2022-07-23 10:02:32 -0300

    Introdução Prática ao TypeScript - Recriando a mesma aplicação simples com TS

--- javascript/Introdução Prática ao TypeScript/monitoria-dio/exemplos/validacao-basica.js
+++ javascript/Introdução Prática ao TypeScript/monitoria-dio/exemplos/validacao-basica.js
@@ -1,3 +1,4 @@
+/*
 //console.log('Estou vinculado ao index.html')
 
 let button = document.getElementById('button')
@@ -16,3 +17,4 @@ function somarNumeros (numero1, numero2){
 button.addEventListener('click', () => {
     console.log(somarNumeros(input1.value, input2.value))
 })
+*/
