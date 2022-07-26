321d1fb3369cfdedd9c5530b1c8c9c76a2fa0270
Caio Thiago <caiothiagos@hotmail.com>
2022-07-26 10:10:28 -0300

    Introdução Prática ao TypeScript - Definindo o tipo de retorno de uma função

--- javascript/Introdução Prática ao TypeScript/monitoria-dio/app.ts
+++ javascript/Introdução Prática ao TypeScript/monitoria-dio/app.ts
@@ -1,13 +1,5 @@
-type input = number | string
-
-function somarValores(input1: input, input2: input){
-    if(typeof input1 === "string" || typeof input2 === "string"){
-        return input1.toString() + input2.toString()
-    }else{
-        return input1 + input2
-    }
+function somaValoresNumericos(numero1: number, numero2: number): number{
+    return numero1 + numero2
 }
 
-console.log(somarValores(1,5))
-console.log(somarValores('ola',', tudo bem?'))
-console.log(somarValores('1',5))
+console.log(somaValoresNumericos(1, 3))
